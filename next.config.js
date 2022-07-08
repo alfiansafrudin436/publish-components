ad128a8dd3a79f00cc937039be0b0b5bdf4bb886
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2022-07-08 14:38:10 +0700

    feat: update components

--- next.config.js
+++ next.config.js
@@ -12,10 +12,44 @@ const moduleExports = {
   webpack: (config) => {
     const conf = config
     conf.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-    conf.module.rules.push({
-      test: /\.txt$/,
-      use: 'raw-loader',
-    })
+    conf.module.rules.push(
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          limit: 1024,
+          name: '[name].[ext]',
+          publicPath: 'dist/assets/',
+          outputPath: 'dist/assets/'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // 3. Inject styles into DOM
+            "css-loader", // 2. Turns css into commonjs
+            "sass-loader", // 1. Turns sass into css
+        ],
+    },
+    )
     return conf
   },
   compiler: {
