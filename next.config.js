8caca37e73152caaba5c744ac3f519ef5d87ec99
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2022-07-08 14:24:07 +0700

    feat: update components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const webpack = require('webpack')
+const { parsed: localEnv } = require('dotenv').config()
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === true,
+})
+
+const moduleExports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    const conf = config
+    conf.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+    conf.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        {
+          loader: '@mdx-js/loader',
+        },
+      ],
+    })
+    return conf
+  },
+  compiler: {
+    styledComponents: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(moduleExports)
