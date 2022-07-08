00711e1de458e2fbd1d476e5a037c048a091554d
alfiansafrudin436 <alfiansafrudin436@gmail.com>
2022-07-08 14:41:38 +0700

    feat: update components

--- next.config.js
+++ next.config.js
@@ -23,32 +23,9 @@ const moduleExports = {
         loader: require.resolve("babel-loader"),
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.png|svg|jpg|gif$/,
         use: ["file-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          limit: 1024,
-          name: '[name].[ext]',
-          publicPath: 'dist/assets/',
-          outputPath: 'dist/assets/'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-            "style-loader", // 3. Inject styles into DOM
-            "css-loader", // 2. Turns css into commonjs
-            "sass-loader", // 1. Turns sass into css
-        ],
-    },
+      }
     )
     return conf
   },
